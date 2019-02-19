f7d2d8ce7a52d01cdf035b3812f34b82056ed69c
billsioros <billsioros97@gmail.com>
2019-02-19 21:47:52 +0200

    Window macros to game

--- include/game.hpp
+++ include/game.hpp
@@ -1,11 +1,6 @@
 
 #pragma once
 
-#define X_WINDOW (0)
-#define Y_WINDOW (0)
-#define N_WINDOW ("Snake")
-#define DELAY (100U)
-
 namespace Game
 {
     void reshape(int width, int height);
--- include/window.hpp
+++ include/window.hpp
@@ -3,6 +3,11 @@
 
 #include <types.hpp>
 
+#define X_WINDOW (0)
+#define Y_WINDOW (0)
+#define N_WINDOW ("Snake")
+#define DELAY (100U)
+
 namespace Game
 {
     namespace Window
